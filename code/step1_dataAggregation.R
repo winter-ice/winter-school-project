63802927549efa47989d41798f0bd2763290d49f
bcdemattei <bcdemattei@gmail.com>
2024-03-13 11:29:46 -0500

    created cleaning and aggregation code, added data

---
+++ code/step1_dataAggregation.R
@@ -0,0 +1,12 @@
+### Title: Data Aggregation for Winter School Project
+### Created by: Braden DeMattei
+### Created on: March 13, 2024
+### Last Updated: March 13, 2024
+
+#### Packages
+library(tidyverse)
+library(lubridate)
+
+### CTD CSV Preparation
+
+
