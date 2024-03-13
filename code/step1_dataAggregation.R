e712af894710952ce071ea2fe35398c6dabe9930
bcdemattei <bcdemattei@gmail.com>
2024-03-13 14:09:30 -0500

    changed data structure

--- code/step1_dataAggregation.R
+++ code/step1_dataAggregation.R
@@ -6,7 +6,21 @@
 #### Packages
 library(tidyverse)
 library(lubridate)
+library(data.table)
 
-### CTD CSV Preparation
+### CTD CSV Preparation Bog Mar12
+
+filenames <- list.files(path = "./data")
+
+##Create list of data frame names without the ".csv" part 
+names <-substr(filenames, 1, nchar(filenames)-4)
+
+###Load all files
+for(i in names){
+  filepath <- file.path("./data",paste(i,".csv",sep=""))
+  assign(i, as_tibble(read.csv(file.path(filepath), skip = 18, header = T)))
+}
+
+###Aggregate Bog Data
 
 
