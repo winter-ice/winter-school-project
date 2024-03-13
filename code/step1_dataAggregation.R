1a94e746d0de8641f2b53af830e670f5df6b99e1
bcdemattei <bcdemattei@gmail.com>
2024-03-13 16:56:45 -0500

    finished aggregation section

--- code/step1_dataAggregation.R
+++ code/step1_dataAggregation.R
@@ -3,18 +3,15 @@
 ### Created on: March 13, 2024
 ### Last Updated: March 13, 2024
 
-#### Packages
+#### Packages & Functions
 library(tidyverse)
 library(lubridate)
 library(data.table)
+library(plyr)
 
-name_to_string <- function(z){
-  nm <-deparse(substitute(z))
-  print(nm)
-}
-### CTD CSV Preparation Bog Mar12
+### CTD CSV Preparation Bog & Lake
 
-filenames <- list.files(path = "./data")
+filenames <- list.files(path = "./data") ## Get filenames
 
 ##Create list of data frame names without the ".csv" part 
 names <-substr(filenames, 1, nchar(filenames)-4)
@@ -23,7 +20,7 @@ names <-substr(filenames, 1, nchar(filenames)-4)
 for(i in names){
   filepath <- file.path("./data",paste(i,".csv",sep=""))
   assign(i, as_tibble(read.csv(file.path(filepath), skip = 18, header = T)))
-}
+} ##Read csv files in and assign variables based off names of files
 
 for(i in names){
   x <- list()
@@ -32,12 +29,18 @@ for(i in names){
     x <- data.frame(x)
     }
    assign(i, cbind(get(i), x))
-   
-}
+} ##Assign the waterbody, zone, holeID, & sampleEventID (combined)
 
+x <- data.frame(NULL) #create empty dataframe for row-binding all datasets
 
+for(i in names){
+  x <- rbind.fill(x, get(i))
+} #row bind all data sets
 
-###Add Differentiation Columns
+aggregated_all_bog_lake <- x #rename dataset
+
+###Because of different CTD probes being used, there are different variable names
+#####This will have to be dealt with at a later date
 
 
 
