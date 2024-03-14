e6bf25eb9fc2a6de91931f19a244a4bdc18faca6
bcdemattei <bcdemattei@gmail.com>
2024-03-14 12:02:09 -0500

    changed column names

--- code/step1_dataAggregation.R
+++ code/step1_dataAggregation.R
@@ -37,10 +37,33 @@ for(i in names){
   x <- rbind.fill(x, get(i))
 } #row bind all data sets
 
-aggregated_all_bog_lake <- x #rename dataset
+agg_all <- as_tibble(x) #rename dataset
 
 ###Because of different CTD probes being used, there are different variable names
 #####This will have to be dealt with at a later date
 
+names(agg_all) <- c("date_time", "turb_ntu_803671", "bga_pc_fluoro_rfu_797015",
+                    "act_cond_mscm_803397", "spec_cond_mscm_803397",
+                    "sal_psu_803397", "resis_ohmcm_803397",
+                    "dens_gcm_803397", "tot_diss_solids_ppt_803397",
+                    "chl_a_fluoro_rfu_803397", "temp_c_804550",
+                    "baro_press_mmhg_804550", "press_psi_785112",
+                    "depth_m_785112", "surf_elev_m_785112",
+                    "ext_volt_v_804550", "batt_capa_804550",
+                    "baro_press_mbar_868976", "temp_c_868976",
+                    "marked", "wbody", "act_cond_mscm_801689",
+                    "spec_cond_mscm_801689", "sal_psu_801689",
+                    "resis_ohmcm_801689", "dens_gcm_801689",
+                    "tot_diss_solids_ppt_801689", "turb_ntu_805029",
+                    "chl_a_fluoro_rfu_804424", "bga_pc_fluoro_rfu_864349",
+                    "temp_c_804569", "baro_press_mmhg_804569",
+                    "press_psi_785110", "depth_ft_785110",
+                    "ext_volt_v_804569", "batt_capa_804569",
+                    "baro_press_mbar_791538", "temp_c_791538",
+                    "lat", "lon", "depth_m_785110")
+head(agg_all)
 
+agg_all$date_time <- lubridate::as_datetime(agg_all$date_time)
+agg_all$zone <- substr(agg_all$wbody, 13, 13)
+agg_all$holeID <- substr(agg_all$wbody, )
 
